dcaa78038f8a587b15fafb166e74b789a0825825
reki <117578469+r0creki@users.noreply.github.com>
2026-02-16 14:26:13 +0800

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
 
   res.setHeader("Set-Cookie", [
-    "token=; HttpOnly; Path=/; Max-Age=0",
-    "workink_pass=; HttpOnly; Path=/; Max-Age=0"
+    "token=; HttpOnly; Path=/; Max-Age=0; Secure; SameSite=Lax",
+    "workink_pass=; HttpOnly; Path=/; Max-Age=0; Secure; SameSite=Lax"
   ]);
 
   res.status(200).json({ success: true });
