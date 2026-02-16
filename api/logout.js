b7a55468ec075789f5206a8c096b46f491b71301
reki <117578469+r0creki@users.noreply.github.com>
2026-02-16 21:09:46 +0800

    Delete api/logout.js

--- api/logout.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-
-  res.setHeader("Set-Cookie", [
-    "token=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=None",
-    "workink_pass=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=None"
-  ]);
-
-  res.status(200).json({ success: true });
-}
