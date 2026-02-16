d9fb19eb415ed9cd3b7d58ad368eb9af95b41d18
reki <117578469+r0creki@users.noreply.github.com>
2026-02-16 14:44:57 +0800

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,8 +1,10 @@
 export default function handler(req, res) {
 
+  const isProd = process.env.NODE_ENV === "production";
+
   res.setHeader("Set-Cookie", [
-    "token=; HttpOnly; Path=/; Max-Age=0; Secure; SameSite=Lax",
-    "workink_pass=; HttpOnly; Path=/; Max-Age=0; Secure; SameSite=Lax"
+    `token=; HttpOnly; Path=/; Max-Age=0; SameSite=Lax${isProd ? "; Secure" : ""}`,
+    `workink_pass=; HttpOnly; Path=/; Max-Age=0; SameSite=Lax${isProd ? "; Secure" : ""}`
   ]);
 
   res.status(200).json({ success: true });
