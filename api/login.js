142554424d372f2ae307775f0d933eca3e7cb3a2
reki <117578469+r0creki@users.noreply.github.com>
2026-02-16 12:12:42 +0800

    Update login.js

---
+++ api/login.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.CLIENT_ID,
+    redirect_uri: process.env.REDIRECT_URI,
+    response_type: "code",
+    scope: "identify"
+  });
+
+  res.redirect(
+    `https://discord.com/oauth2/authorize?${params}`
+  );
+}
